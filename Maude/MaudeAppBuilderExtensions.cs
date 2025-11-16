49dd6d50077090512d491fccc85059583bb1a5fb
Matthew Robbins <matthew.ch.robbins@gmail.com>
2025-11-16 15:04:55 +1100

    Demo images. Do not auto activate Maude in the app builder. Require explicit Activate.

--- Maude/MaudeAppBuilderExtensions.cs
+++ Maude/MaudeAppBuilderExtensions.cs
@@ -8,34 +8,22 @@ namespace Maude;
 /// </summary>
 public static class MaudeAppBuilderExtensions
 {
-
     /// <summary>
     /// Set's up the <see cref="MauiAppBuilder"/> to initialise the <see cref="MaudeRuntime"/> and registers required fonts and dependencies. 
     /// </summary>
-    public static MauiAppBuilder UseMaude(this MauiAppBuilder builder, bool activate =  true)
+    public static MauiAppBuilder UseMaude(this MauiAppBuilder builder)
     {
-        builder.ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("MaterialSymbolsOutlined.ttf", MaudeConstants.MaterialSymbolsFontName);
-        });
-
-        var didInitialise = false;
         if (!MaudeRuntime.IsInitialized)
         {
             MaudeRuntime.Initialize();
-            didInitialise = true;
         }
 
-        if (activate)
-        {
-            MaudeRuntime.Activate();
-        }
-        
         builder.Services.AddSingleton<IMaudeRuntime>(_ => MaudeRuntime.Instance);
         builder.Services.AddSingleton<IMaudeDataSink>(_ => MaudeRuntime.Instance.DataSink);
         
-        return builder.UseSkiaSharp();
+        return builder.ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("MaterialSymbolsOutlined.ttf", MaudeConstants.MaterialSymbolsFontName);
+        }).UseSkiaSharp();
     }
-
-    
 }
